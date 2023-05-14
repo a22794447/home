a6e76d9366e23d044e99d8e612cf8b193329c763
SXuerou <a22794447@gmail.com>
2023-05-14 14:37:54 +0800

    JS 基礎變更

--- dist/js/js_base.js
+++ dist/js/js_base.js
@@ -3,5 +3,5 @@ let firstName="Xuerou";
 let lastName="ars";
 let country="tw";
 
-console.log('(' + country + ')' + firstName + ' ' + lastName)
+console.log('(' + country + ') ' + firstName + ' ' + lastName)
 console.log(`(${country}) ${firstName} ${lastName}`)

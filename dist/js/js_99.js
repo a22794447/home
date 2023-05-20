7da4bcc36e9060bb3e82b11babfee424644f1a38
SXuerou <a22794447@gmail.com>
2023-05-20 10:38:46 +0800

    數值&綁定事件

--- dist/js/js_99.js
+++ dist/js/js_99.js
@@ -3,18 +3,25 @@
 // 抓取
 // 單一
 let main = document.querySelector('#main')
-
 console.log(main)
 
 // 控制內容 (後加載)
-main.innerHTML = 'Super man!'
+// main.innerHTML = 'Super man!'
 
 let lis = document.querySelector('#menu li')
-
 console.log(lis)
 
 // 多選
 let liall = document.querySelectorAll('#menu li')
-
 console.log(liall)
+
+// 數值
+let num1 = document.querySelector('#num-1');
+// num1.value = 'abc';
+// 事件綁定
+let tchBtn = document.querySelector('#tch-btn');
+tchBtn.addEventListener('click', () => {
+    num1.value = 'ABC';
+});
+
 

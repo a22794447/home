a5f2d9adbd60a5153cac2d10093e7fde8f5ddc00
SXuerou <a22794447@gmail.com>
2023-05-20 09:59:13 +0800

    抓取 : 單選/多選

---
+++ dist/js/js_99.js
@@ -0,0 +1,16 @@
+// alert(123)
+
+// 抓取
+// 單一
+let main = document.querySelector('#main')
+
+console.log(main)
+
+let lis = document.querySelector('#menu li')
+
+console.log(lis)
+
+// 多選
+let liall = document.querySelectorAll('#menu li')
+
+console.log(liall)

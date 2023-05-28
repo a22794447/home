4204974e4a5d19cd957e3edfd207c89c99405894
SXuerou <a22794447@gmail.com>
2023-05-28 15:05:35 +0800

    打招呼實做

---
+++ dist/js/say_hi.js
@@ -0,0 +1,21 @@
+const sayHI = () => {
+    let name = document.querySelector('#name');
+    if (!name) {
+        alert('沒有輸入框');
+        return;
+    }
+
+    if (!name.value) {
+        alert('請輸入名字');
+        return;
+    }
+
+    let response = document.querySelector('#response');
+    response.innerHTML = `HI!,${name.value}`;
+    name.value = '';
+    name.focus();
+}
+
+let hi = document.querySelector('#hi');
+
+hi.addEventListener('click', sayHI);

c9c93d651fdb4bc9adb53e047c5fd40aa622fdef
SXuerou <a22794447@gmail.com>
2023-05-28 17:35:20 +0800

    使用 keyup 判斷 enter 觸發

--- dist/js/say_hi.js
+++ dist/js/say_hi.js
@@ -22,13 +22,27 @@ hi.addEventListener('click', sayHI);
 
 let name = document.querySelector('#name');
 
-name.addEventListener('change', () => {
-    if (!name.value) {
-        alert('請輸入名字');
-        name.focus();
-    }
-})
+// name.addEventListener('change', () => {
+//     if (!name.value) {
+//         alert('請輸入名字');
+//         name.focus();
+//     }
+// })
 
-name.addEventListener('blur', () => {
-    console.log(`blur: ${name.value}`);
+// name.addEventListener('blur', () => {
+//     console.log(`blur: ${name.value}`);
+// })
+
+// name.addEventListener('keypress', () => {
+//     console.log(`keypress: ${name.value}`);
+// })
+
+// name.addEventListener('keyup', () => {
+//     console.log(`keypress: ${name.value}`);
+// })
+
+name.addEventListener('keyup', (e) => {
+    if (e.key == 'Enter') {
+        sayHI();
+    }
 })

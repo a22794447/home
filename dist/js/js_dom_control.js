484111174f7e255e43bf7eecd7ae15607c2d1f44
SXuerou <a22794447@gmail.com>
2023-05-28 13:11:40 +0800

    使用 keyup 判斷 enter 觸發

--- dist/js/js_dom_control.js
+++ dist/js/js_dom_control.js
@@ -7,3 +7,15 @@ doing.addEventListener('click', () => {
     uid.style.fontSize = '50px';
     uid.style.backgroundColor = 'black';
 })
+
+let addClass = document.querySelector('#add-class');
+
+addClass.addEventListener('click', () => {
+    uid.classList.add('active');
+})
+
+let removeClass = document.querySelector('#remove-class');
+
+removeClass.addEventListener('click', () => {
+    uid.classList.remove('active');
+})

549298cb1ebbb3b31a623e8c83d7329dd6b7fe67
SXuerou <a22794447@gmail.com>
2023-06-04 17:08:06 +0800

    按鈕與輸入框事件綁定重整

--- dist/js/todo.js
+++ dist/js/todo.js
@@ -2,7 +2,8 @@ const init = () => {
     let addBtn = document.querySelector('#add-btn');
     let itemName = document.querySelector('#item-name');
 
-    addBtn.addEventListener('click', () => {
+    // 新增項目統一收納
+    const doAddItem = () => {
         let value = itemName.value;
 
         if (!value) {
@@ -12,20 +13,16 @@ const init = () => {
         }
 
         console.log(value);
+    }
+
+    addBtn.addEventListener('click', () => {
+        doAddItem();
     });
 
     itemName.addEventListener('keyup', (e) => {
         if (e.key == 'Enter') {
-            let value = itemName.value;
-
-            if (!value) {
-                alert('請輸入項目名稱');
-                itemName.focus();
-                return;
-            }
-
-            console.log(value);
-        }
+         doAddItem();
+         }
     });
 };
 
